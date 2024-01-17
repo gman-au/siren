5c5c37a97d19fdb15cafdf05d7ab5e472e0672d4
Gabriel Palma <gubpalma@hotmail.com>
2024-01-17 15:50:43 +1100

    Adjusted tabs

--- src/6.0/Siren.Infrastructure.Io/FileWriter.cs
+++ src/6.0/Siren.Infrastructure.Io/FileWriter.cs
@@ -5,121 +5,121 @@ using Siren.Infrastructure.Mermaid;
 
 namespace Siren.Infrastructure.Io
 {
-	public static class FileWriter
-	{
-		public static void Perform(
-			string filePath,
-			StringBuilder result,
-			string markdownAnchor = null
-		)
-		{
-			var exists =
-				File
-					.Exists(filePath);
+    public static class FileWriter
+    {
+        public static void Perform(
+            string filePath,
+            StringBuilder result,
+            string markdownAnchor = null
+        )
+        {
+            var exists =
+                File
+                    .Exists(filePath);
 
-			if (!exists)
-			{
-				Console
-					.WriteLine($"Existing file \"{filePath}\" not found, creating...");
-				
-				OverwriteFileContents(
-					filePath,
-					result.ToString()
-				);
-				return;
-			}
-			
-			if (string.IsNullOrEmpty(markdownAnchor))
-			{
-				Console
-					.WriteLine($"Markdown anchor \"{markdownAnchor}\"not specified, writing directly...");
-				
-				OverwriteFileContents(
-					filePath,
-					result.ToString()
-				);
-				return;
-			}
+            if (!exists)
+            {
+                Console
+                    .WriteLine($"Existing file \"{filePath}\" not found, creating...");
 
-			var fileString =
-				File
-					.ReadAllText(filePath);
+                OverwriteFileContents(
+                    filePath,
+                    result.ToString()
+                );
+                return;
+            }
 
-			// First occurrence of markdown anchor
-			var markdownPosition =
-				fileString
-					.IndexOf(
-						markdownAnchor,
-						StringComparison.InvariantCultureIgnoreCase
-					) + markdownAnchor.Length;
+            if (string.IsNullOrEmpty(markdownAnchor))
+            {
+                Console
+                    .WriteLine($"Markdown anchor \"{markdownAnchor}\"not specified, writing directly...");
 
-			if (markdownPosition < 0)
-			{
-				Console
-					.WriteLine($"Markdown anchor \"{markdownAnchor}\"not found in file; overwriting...");
-				
-				OverwriteFileContents(
-					filePath,
-					result.ToString()
-				);
-				return;
-			}
+                OverwriteFileContents(
+                    filePath,
+                    result.ToString()
+                );
+                return;
+            }
 
-			// First occurrence of siren start header, otherwise straight after anchor
-			var sirenAnchorStartPosition =
-				fileString
-					.IndexOf(
-						MermaidConstants.SirenAnchorStart,
-						markdownPosition,
-						StringComparison.InvariantCultureIgnoreCase
-					);
+            var fileString =
+                File
+                    .ReadAllText(filePath);
 
-			if (sirenAnchorStartPosition < 0)
-				sirenAnchorStartPosition = markdownPosition;
-			
-			// First occurrence of siren end header, otherwise straight after anchor
-			var sirenAnchorEndPosition =
-				fileString
-					.IndexOf(
-						MermaidConstants.SirenAnchorEnd,
-						sirenAnchorStartPosition,
-						StringComparison.InvariantCultureIgnoreCase
-					);
+            // First occurrence of markdown anchor
+            var markdownPosition =
+                fileString
+                    .IndexOf(
+                        markdownAnchor,
+                        StringComparison.InvariantCultureIgnoreCase
+                    ) + markdownAnchor.Length;
 
-			// +2 for length of carriage return (otherwise appends line feed each re-write)
-			var lengthToTrim = MermaidConstants.SirenAnchorEnd.Length + 2;
-			
-			if (sirenAnchorEndPosition < 0)
-			{
-				sirenAnchorEndPosition = sirenAnchorStartPosition;
-				lengthToTrim = 0;
-			}
+            if (markdownPosition < 0)
+            {
+                Console
+                    .WriteLine($"Markdown anchor \"{markdownAnchor}\"not found in file; overwriting...");
 
-			fileString =
-				fileString
-					.Remove(
-						sirenAnchorStartPosition,
-						sirenAnchorEndPosition - sirenAnchorStartPosition + lengthToTrim 
-					);
+                OverwriteFileContents(
+                    filePath,
+                    result.ToString()
+                );
+                return;
+            }
 
-			fileString =
-				fileString
-					.Insert(
-						sirenAnchorStartPosition,
-						result.ToString()
-					);
-			
-			OverwriteFileContents(
-				filePath,
-				fileString
-			);
-		}
+            // First occurrence of siren start header, otherwise straight after anchor
+            var sirenAnchorStartPosition =
+                fileString
+                    .IndexOf(
+                        MermaidConstants.SirenAnchorStart,
+                        markdownPosition,
+                        StringComparison.InvariantCultureIgnoreCase
+                    );
 
-		private static void OverwriteFileContents(string filePath, string result) =>
-			File
-				.WriteAllText(
-					filePath,
-					result
-				);
-	}
+            if (sirenAnchorStartPosition < 0)
+                sirenAnchorStartPosition = markdownPosition;
+
+            // First occurrence of siren end header, otherwise straight after anchor
+            var sirenAnchorEndPosition =
+                fileString
+                    .IndexOf(
+                        MermaidConstants.SirenAnchorEnd,
+                        sirenAnchorStartPosition,
+                        StringComparison.InvariantCultureIgnoreCase
+                    );
+
+            // +2 for length of carriage return (otherwise appends line feed each re-write)
+            var lengthToTrim = MermaidConstants.SirenAnchorEnd.Length + 2;
+
+            if (sirenAnchorEndPosition < 0)
+            {
+                sirenAnchorEndPosition = sirenAnchorStartPosition;
+                lengthToTrim = 0;
+            }
+
+            fileString =
+                fileString
+                    .Remove(
+                        sirenAnchorStartPosition,
+                        sirenAnchorEndPosition - sirenAnchorStartPosition + lengthToTrim
+                    );
+
+            fileString =
+                fileString
+                    .Insert(
+                        sirenAnchorStartPosition,
+                        result.ToString()
+                    );
+
+            OverwriteFileContents(
+                filePath,
+                fileString
+            );
+        }
+
+        private static void OverwriteFileContents(string filePath, string result) =>
+            File
+                .WriteAllText(
+                    filePath,
+                    result
+                );
+    }
 }
--- src/6.0/Siren.Tool/Program.cs
+++ src/6.0/Siren.Tool/Program.cs
@@ -6,10 +6,10 @@ using Siren.Infrastructure.Mermaid;
 using Siren.Infrastructure.Poco;
 
 Console
-	.WriteLine("Starting Siren console...");
+    .WriteLine("Starting Siren console...");
 
 if (args.Length < 2)
-	throw new Exception("Expected 2 arguments");
+    throw new Exception("Expected 2 arguments");
 
 var assemblyPath = args[0];
 var outputPath = args[1];
@@ -17,41 +17,42 @@ var markdownAnchor = (string)null;
 
 if (args.Length > 2)
 {
-	markdownAnchor = args[2];
+    markdownAnchor = args[2];
 }
 
 if (string.IsNullOrEmpty(assemblyPath))
-	throw new Exception("Assembly path argument invalid");
+    throw new Exception("Assembly path argument invalid");
 
 if (string.IsNullOrEmpty(outputPath))
-	throw new Exception("Output path argument invalid");
+    throw new Exception("Output path argument invalid");
 
 Assembly assembly;
 
 try
 {
-	assembly =
-		Assembly
-			.LoadFile(assemblyPath);
+    assembly =
+        Assembly
+            .LoadFile(assemblyPath);
 }
 catch (FileNotFoundException)
 {
-	throw new Exception($"Could not load assembly from {assemblyPath}");
+    throw new Exception($"Could not load assembly from {assemblyPath}");
 }
 
 var universe =
-	AssemblyScanner
-		.Perform(assembly);
+    AssemblyScanner
+        .Perform(assembly);
 
 var result =
-	MermaidRenderer
-		.Perform(universe);
+    MermaidRenderer
+        .Perform(universe);
 
 FileWriter
-	.Perform(
-		outputPath,
-		result,
-		markdownAnchor);
-		
+    .Perform(
+        outputPath,
+        result,
+        markdownAnchor
+    );
+
 Console
-	.WriteLine("Siren operation completed");
+    .WriteLine("Siren operation completed");
