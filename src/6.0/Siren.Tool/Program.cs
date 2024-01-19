5c90c658a42f44b280945797feba574ac4fe8f99
Gabriel Palma <gubpalma@hotmail.com>
2024-01-19 15:43:07 +1100

    Scanner selector based on assembly type

--- src/6.0/Siren.Tool/Program.cs
+++ src/6.0/Siren.Tool/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Reflection;
+using Siren.Domain;
 using Siren.Infrastructure.AssemblyLoad;
 using Siren.Infrastructure.Io;
 using Siren.Infrastructure.Mermaid;
@@ -27,22 +28,42 @@ if (string.IsNullOrEmpty(outputPath))
     throw new Exception("Output path argument invalid");
 
 Assembly assembly;
+var isASnapshotAssembly = false;
 
 try
 {
     assembly =
         Assembly
             .LoadFrom(assemblyPath);
+
+    isASnapshotAssembly = 
+        assembly
+            .IsASnapshotAssembly();
 }
 catch (FileNotFoundException)
 {
     throw new Exception($"Could not load assembly from {assemblyPath}");
 }
 
-var universe =
-    // AssemblyScanner
-    SnapshotAssemblyScanner
-        .Perform(assembly);
+Universe universe;
+
+if (isASnapshotAssembly) {
+    Console
+        .WriteLine("ModelSnapshot type found in assembly... scanning...");
+    
+    universe =
+        SnapshotAssemblyScanner
+            .Perform(assembly);
+}
+else
+{
+    Console
+        .WriteLine("POCO default assembly... scanning...");
+    
+    universe =
+        PocoAssemblyScanner
+            .Perform(assembly);
+}
 
 var result =
     MermaidRenderer
